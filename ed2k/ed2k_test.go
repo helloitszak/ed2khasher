27379205c92d92ca30149c9b3cb801c2ef4c4e00
Yuki <aaron.ahmed.eve@gmail.com>
2015-02-19 00:08:50 +0900

    Wrap repeated code in a function

--- ed2k/ed2k_test.go
+++ ed2k/ed2k_test.go
@@ -5,74 +5,47 @@ import (
 	"testing"
 )
 
-func TestSmallFile(t *testing.T) {
-	test := bytes.NewReader(make([]byte, 600))
-	validHash := "a5b489c18c5bdc1f711a8edff22c13ff"
-	testHash, _ := Hash(test, false)
-	if testHash != validHash {
-		t.Error("Expected ", validHash, " got ", testHash)
+func HashWrapper(t *testing.T, size int, hash string, old bool) {
+	test := bytes.NewReader(make([]byte, size))
+	testHash, err := Hash(test, old)
+	
+	if err != nil {
+		t.Error("Got error ", err)
 	}
+	
+	if testHash != hash {
+		t.Error("Expected ", hash, " got ", testHash)
+	}
+}
+
+func TestSmallFile(t *testing.T) {
+	HashWrapper(t, 600, "a5b489c18c5bdc1f711a8edff22c13ff", false)
 }
 
 func TestSmallFileOld(t *testing.T) {
-	test := bytes.NewReader(make([]byte, 600))
-	validHash := "a5b489c18c5bdc1f711a8edff22c13ff"
-	testHash, _ := Hash(test, true)
-	if testHash != validHash {
-		t.Error("Expected ", validHash, " got ", testHash)
-	}
+	HashWrapper(t, 600, "a5b489c18c5bdc1f711a8edff22c13ff", true)
 }
 
 func TestEqualFile(t *testing.T) {
-	test := bytes.NewReader(make([]byte, 9728000))
-	validHash := "d7def262a127cd79096a108e7a9fc138"
-	testHash, _ := Hash(test, false)
-	if testHash != validHash {
-		t.Error("Expected ", validHash, " got ", testHash)
-	}
+	HashWrapper(t, 9728000, "d7def262a127cd79096a108e7a9fc138", false)
 }
 
 func TestEqualFileOld(t *testing.T) {
-	test := bytes.NewReader(make([]byte, 9728000))
-	validHash := "fc21d9af828f92a8df64beac3357425d"
-	testHash, _ := Hash(test, true)
-	if testHash != validHash {
-		t.Error("Expected ", validHash, " got ", testHash)
-	}
+	HashWrapper(t, 9728000, "fc21d9af828f92a8df64beac3357425d", true)
 }
 
 func TestMultipleFile(t *testing.T) {
-	test := bytes.NewReader(make([]byte, 19456000))
-	validHash := "194ee9e4fa79b2ee9f8829284c466051"
-	testHash, _ := Hash(test, false)
-	if testHash != validHash {
-		t.Error("Expected ", validHash, " got ", testHash)
-	}
+	HashWrapper(t, 19456000, "194ee9e4fa79b2ee9f8829284c466051", false)
 }
 
 func TestMultipleFileOld(t *testing.T) {
-	test := bytes.NewReader(make([]byte, 19456000))
-	validHash := "114b21c63a74b6ca922291a11177dd5c"
-	testHash, _ := Hash(test, true)
-	if testHash != validHash {
-		t.Error("Expected ", validHash, " got ", testHash)
-	}
+	HashWrapper(t, 19456000, "114b21c63a74b6ca922291a11177dd5c", true)
 }
 
 func TestLargeFile(t *testing.T) {
-	test := bytes.NewReader(make([]byte, 19457000))
-	validHash := "345da2ffa0f63eae5638b908f187bfb1"
-	testHash, _ := Hash(test, false)
-	if testHash != validHash {
-		t.Error("Expected ", validHash, " got ", testHash)
-	}
+	HashWrapper(t, 19457000, "345da2ffa0f63eae5638b908f187bfb1", false)
 }
 
 func TestLargeFileOld(t *testing.T) {
-	test := bytes.NewReader(make([]byte, 19457000))
-	validHash := "345da2ffa0f63eae5638b908f187bfb1"
-	testHash, _ := Hash(test, true)
-	if testHash != validHash {
-		t.Error("Expected ", validHash, " got ", testHash)
-	}
+	HashWrapper(t, 19457000, "345da2ffa0f63eae5638b908f187bfb1", true)
 }
